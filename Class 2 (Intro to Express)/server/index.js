1bcf49b38419f4512bcc62e0f963f93fa77f5b22
Tanishq217 <tanishqsinghtanishq135@gmail.com>
2025-08-13 15:11:37 +0530

    Update > Routes

--- Class 2 (Intro to Express)/server/index.js
+++ Class 2 (Intro to Express)/server/index.js
@@ -15,16 +15,41 @@ const app = express() ; // Calling the express !
 
 
 
+// Creating home Route and about route
 
-
-// Creating home 
-
-app.get('/' , (req , res) =>{
+app.get('/home' , (req , res) =>{
   res.send('Home Page Data') // we can put HTML data here // or website data
 })
 
 app.get('/about' , (req , res) =>{
   res.send('Home About Data') // we can put HTML data here // or website data
+})
+
+
+// Making Dummy DataBase for Course Route 
+let courses = [
+  { id: 1, name: 'JavaScript Basics' },
+  { id: 2, name: 'Express.js Fundamentals' },
+  { id: 3, name: 'Node.js Advanced' }
+]
+
+// COURSE ROUT 
+app.get('/courses' , (req , res) =>{
+  // when we hit "Courses" , we should see entire set of courses 
+  res.send(courses) 
+}) 
+
+// Route Parameters 
+// How to set up Route Parameters >> ADD ":"
+
+
+// Specific Course Route by Course ID
+
+app.get('/courses/:id' , (req , res) =>{
+  // console.log(req.params)
+  // res.send(courses)
+
+  res.send(courses.find(course => course.id === parseInt(req.params.id))) // we can use parseInt to convert string to number
 })
 
 app.listen (8001 , () => {
